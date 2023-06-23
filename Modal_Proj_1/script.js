04f83449f96cbb893ebef7d803fc959629dbbec1
Jason McGrath <jnomad21@yahoo.com>
2023-06-23 16:24:12 -0700

    close modal functionality on clicking overlay, DRY into a single function

--- Modal_Proj_1/script.js
+++ Modal_Proj_1/script.js
@@ -8,12 +8,11 @@ for(let i=0; i<btnsOpenModal.length; i++){
     overlay.classList.remove('hidden')
     })
 }
-btnCloseModal.addEventListener('click', function(){
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
-})
 
-overlay.addEventListener('click', function(){
+const closeModal = function(){
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
-})
+}
+btnCloseModal.addEventListener('click', closeModal)
+    
+overlay.addEventListener('click', closeModal)

153615ec2b054a3ed82ae74e31a21c4c3ecfff9a
Jason McGrath <jnomad21@yahoo.com>
2023-06-26 12:39:10 -0700

    switching active player

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,3 +1,5 @@
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El=document.querySelector("#score--0");
 const score1El=document.getElementById("score--1");
 const current0El=document.getElementById('current--0')
@@ -8,10 +10,13 @@ const btnRoll=document.querySelector('.btn--roll');
 const btnHold=document.querySelector('.btn--hold');
 
 
-score0El.textContent=0
-score1El.textContent=0
+score0El.textContent= 0
+score1El.textContent= 0
 diceEl.classList.add('hidden')
+
+const scores = [0,0]
 let currentScore = 0
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function(){
     const dice = Math.trunc(Math.random()*6) + 1
@@ -19,9 +24,16 @@ btnRoll.addEventListener('click', function(){
     diceEl.src =`dice-${dice}.png`
     if (dice !== 1){
         currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
         current0El.textContent=currentScore
 
     }else{
+        document.getElementById(`current--${activePlayer}`).textContent = 0
+        currentScore = 0
+        activePlayer = activePlayer === 0 ? 1 : 0;
+
+        
+
         
     }
 })

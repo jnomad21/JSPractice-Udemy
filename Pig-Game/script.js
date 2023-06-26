26729a6d34b5f0c05991ec8571e643181175f365
Jason McGrath <jnomad21@yahoo.com>
2023-06-26 13:21:56 -0700

    score hold functionality

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -18,6 +18,14 @@ const scores = [0,0]
 let currentScore = 0
 let activePlayer = 0;
 
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
+};
+
 btnRoll.addEventListener('click', function(){
     const dice = Math.trunc(Math.random()*6) + 1
     diceEl.classList.remove('hidden')
@@ -25,17 +33,25 @@ btnRoll.addEventListener('click', function(){
     if (dice !== 1){
         currentScore += dice;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore
-        current0El.textContent=currentScore
+        
 
     }else{
-        document.getElementById(`current--${activePlayer}`).textContent = 0
-        currentScore = 0
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active')
-        player1El.classList.toggle('player--active')
-
-        
-
+        switchPlayer()
         
     }
+
+});
+
+btnHold.addEventListener('click', function(){
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    
+    if (scores[activePlayer]>=10){
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+
+    }else{
+        switchPlayer
+    }
+    
 })
